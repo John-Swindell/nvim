9696f8507c6567e099b6d68702f3b14abe589b1e
swindellj-fla <swindellj@floridapanthers.com>
2026-09-11 15:09:24 -0400

    i don't remember what this was, but ts working rn so, we ball. At least there is a .bak for something...

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,18 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  { -- File-history / commit-diff viewer (VS Code-style "timeline" and per-commit diffs).
+    -- Commit *list* browsing is handled by Telescope's git_commits/git_bcommits pickers
+    -- (<leader>gc / <leader>gb, set up in init.lua); Diffview renders what you select there.
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory', 'DiffviewClose' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view (working tree vs HEAD)' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory<cr>', desc = '[G]it [H]istory (whole repo)' },
+      { '<leader>gf', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [F]ile history (current buffer)' },
+      { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it [Q]uit diffview' },
+    },
+    opts = {},
+  },
+}
